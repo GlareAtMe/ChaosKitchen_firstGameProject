4e120691a7f15ed61e3fdbeb401e718e6ccd78e2
GlareAtMe <ronpo.amx.st@gmail.com>
2025-04-14 23:57:46 +0300

    Configure Git LFS for Unity assets

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+    [SerializeField] private Button playAgainButton;
+
+    public event EventHandler OnRestartGame;
 
     private void Start() {
         GameManager.Instance.OnStateChange += GameManager_OnStateChange;
@@ -18,6 +23,9 @@ public class GameOverUI : MonoBehaviour
             Show();
 
             recipesDeliveredText.text = DeliveryManager.Instance.GetAmountOfSuccessRecepes().ToString();
+            playAgainButton.onClick.AddListener(() => {
+                OnRestartGame?.Invoke(this, EventArgs.Empty);
+            });
         } else {
             Hide();
         }
